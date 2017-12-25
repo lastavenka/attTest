40d9aa0cf85cd47c54fa56d3e59f7c5a21e63418
Natallia Lastavenka <natasha.lastavenka@gmail.com>
2017-12-25 22:47:10 +0300

    Working version

--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,14 +1,36 @@
 'use strict';
 
-var hotelsTitle = document.querySelector('#hotels');
-var sanatoriumsTitle = document.querySelector('#sanatoriums');
-var recreationСentersTitle = document.querySelector('#recreationСenters');
+var contentTitles = document.querySelectorAll('.content__item-title');
 var nav = document.querySelector('.page-header__nav');
-var menu = document.querySelector('.menu__list');
-var headerHeight = getComputedStyle(document.querySelector('.page-header')).height.slice(0, -2);
+var menu = document.querySelector('.menu');
+var headerHeight = document.querySelector('.page-header').offsetHeight;
+var footer = document.querySelector('.page-footer');
 var headerPadding = 45;
-var hamburger = document.querySelector(".hamburger");
-var mainNav = document.querySelector(".page-header__nav");
+var hamburger = document.querySelector('.hamburger');
+var mainNav = document.querySelector('.page-header__nav');
+var tabletsPortraitWidth = 900;
+var s = !0;
+
+var offset = function(a) {
+  for (var b = 0; a; ) (b += parseInt(a.offsetTop)), (a = a.offsetParent);
+  return b;
+};
+
+window.onload = function() {
+  var a = menu,
+    b = offset(a),
+    f = window.getComputedStyle ? getComputedStyle(a, '') : a.currentStyle,
+    d = a.offsetHeight + parseInt(f.marginTop) || 0,
+    e = offset(footer);
+  window.onscroll = function() {
+    var c = window.pageYOffset || document.documentElement.scrollTop,
+      c = e - (c + d + b);
+    s != 0 < c &&
+      ((s = 0 < c)
+        ? ((a.style.top = b + 'px'), (a.style.position = 'fixed'))
+        : ((a.style.top = e - d + 'px'), (a.style.position = 'absolute')));
+  };
+};
 
 var scrollToHeader = function(elem) {
   var selectedPosY = 0;
@@ -23,13 +45,39 @@ var scrollToHeader = function(elem) {
 
 var onNavClick = function(e) {
   e.preventDefault();
+
   if (e.target.tagName == 'A') {
-    var elem = eval(e.target.getAttribute('data-nav') + "Title");
-    scrollToHeader(elem);
-    hamburger.classList.toggle("active");
-    mainNav.classList.toggle("page-header__nav--close");
+    scrollToHeader(
+      document.querySelector('#' + e.target.getAttribute('data-nav'))
+    );
+
+    if (document.documentElement.offsetWidth < tabletsPortraitWidth) {
+      hamburger.classList.toggle('active');
+      mainNav.classList.toggle('page-header__nav--close');
+    }
   }
 };
 
+var onContentScroll = function() {
+  [].forEach.call(contentTitles, function(elem) {
+    if (
+      elem.getBoundingClientRect().top <=
+      +headerHeight + +headerPadding * 5
+    ) {
+      menu
+        .querySelector('.menu__link--active')
+        .classList.remove('menu__link--active');
+      menu
+        .querySelector(
+          '.menu__link[data-nav="' + elem.getAttribute('id') + '"]'
+        )
+        .classList.add('menu__link--active');
+    }
+  });
+};
+
+if (document.documentElement.offsetWidth > tabletsPortraitWidth) {
+  window.addEventListener('scroll', onContentScroll);
+}
 nav.addEventListener('click', onNavClick);
 menu.addEventListener('click', onNavClick);
